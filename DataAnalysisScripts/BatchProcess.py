6892973e859384d0406f61a357601842a2b10ee5
Delphine <midelf17@gmail.com>
2019-06-28 12:00:00 -0700

    Removed manual input, Added CSV file for batch processing data

--- DataAnalysisScripts/BatchProcess.py
+++ DataAnalysisScripts/BatchProcess.py
@@ -16,40 +16,20 @@ import matplotlib.pyplot as plt
 # For inline plot
 #get_ipython().run_line_magic('matplotlib', 'inline')
 from tkinter import filedialog
+import pandas as pd
 
+batch_file = "C:/Users/Hongquan/Documents/BatchFile.csv"
 
-FileList = {}
+df_batch = pd.read_csv(batch_file)
 
-counter = 0
-while 1:
-
-    Tmin = float(input('Enter Tmin value: '))
-    Tmax = float(input('Enter Tmax value: '))
-    File = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("CSV files","*.csv"),("all files","*.*")))
-    Organism = input('Enter organism name: ')
-    Condition = input('Enter track condition: ')
-    Size = float(input('Enter organism size in mm: '))
-    FileList[counter] = [Tmin, Tmax, File, Organism, Condition, Size]
-    counter += 1
+for ii in range(len(df_batch)):
     
-    c = input('Press q now to exit, c to continue: ')
-    
-    print(c)
-
-    if c == 'q':
-        break
-
-    
-print(FileList)
-
-for ii in range(len(FileList)):
-    
-    Tmin = FileList[ii][0]
-    Tmax = FileList[ii][1]
-    FileName = FileList[ii][2]
-    Organism = FileList[ii][3]
-    Condition = FileList[ii][4]
-    Size = FileList[ii][5]
+    Tmin = df_batch['Tmin'][ii]
+    Tmax = df_batch['Tmax'][ii]
+    FileName = df_batch['FileName'][ii]
+    Organism = df_batch['Organism'][ii]
+    Condition = df_batch['Condition'][ii]
+    Size = df_batch['Size'][ii]
     
     print(Tmin)
     print(Tmax)
@@ -57,11 +37,54 @@ for ii in range(len(FileList)):
     print(Organism)
     print(Condition)
     print(Size)
-    
     track = GravityMachineTrack.gravMachineTrack(fileName = FileName, organism = Organism, condition = Condition, Tmin = Tmin, Tmax = Tmax, computeDisp = True, orgDim = Size, overwrite_piv=False, overwrite_velocity=False)
-
+#
     track.saveAnalysisData(overwrite = True)
-    
+
+#FileList = {}
+#
+#counter = 0
+#while 1:
+#
+#    Tmin = float(input('Enter Tmin value: '))
+#    Tmax = float(input('Enter Tmax value: '))
+#    File = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("CSV files","*.csv"),("all files","*.*")))
+#    Organism = input('Enter organism name: ')
+#    Condition = input('Enter track condition: ')
+#    Size = float(input('Enter organism size in mm: '))
+#    FileList[counter] = [Tmin, Tmax, File, Organism, Condition, Size]
+#    counter += 1
+#    
+#    c = input('Press q now to exit, c to continue: ')
+#    
+#    print(c)
+#
+#    if c == 'q':
+#        break
+#
+#    
+#print(FileList)
+#
+#for ii in range(len(FileList)):
+#    
+#    Tmin = FileList[ii][0]
+#    Tmax = FileList[ii][1]
+#    FileName = FileList[ii][2]
+#    Organism = FileList[ii][3]
+#    Condition = FileList[ii][4]
+#    Size = FileList[ii][5]
+#    
+#    print(Tmin)
+#    print(Tmax)
+#    print(FileName)
+#    print(Organism)
+#    print(Condition)
+#    print(Size)
+#    
+#    track = GravityMachineTrack.gravMachineTrack(fileName = FileName, organism = Organism, condition = Condition, Tmin = Tmin, Tmax = Tmax, computeDisp = True, orgDim = Size, overwrite_piv=False, overwrite_velocity=False)
+#
+#    track.saveAnalysisData(overwrite = True)
+#    
     
     
 
