e0ec8480235fddfde6d4bdaf7bbd9b23ca7e6990
Deepak Krishnamurthy <deepak90@stanford.edu>
2021-02-18 21:14:43 -0800

    Added _def file for definitions and constants

---
+++ DataAnalyserGui/_def.py
@@ -0,0 +1,14 @@
+# Definitions and constants
+# Map between CSV headers and internal state variables of the GUI
+
+VARIABLE_HEADER_MAPPING = {'Time':'Time', 'X_obj':'Y_stage','Y_obj':'X_stage','Z_obj':'Z_stage','Image name':'DF1', 'X_image':'X_image', 'Z_image':'Y_image'}
+
+class Chamber:
+
+	WIDTH = 5
+	LENGTH = 30
+	def __init__(self):
+		pass
+
+		
+
