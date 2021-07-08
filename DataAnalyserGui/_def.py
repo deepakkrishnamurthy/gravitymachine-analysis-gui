855ae1484114dd2e04e434935400fa4235cc09b7
Deepak <deepak90@stanford.edu>
2021-07-07 19:01:15 -0700

    Fixed opencv version to avoid dependency issues with GUI

--- DataAnalyserGui/_def.py
+++ DataAnalyserGui/_def.py
@@ -2,10 +2,10 @@
 # Map between CSV headers and internal state variables of the GUI
 
 # Newer variable mappings (For GM codebase >= v2.0)
-# VARIABLE_HEADER_MAPPING = {'Time':'Time', 'X_obj':'X_objStage','Y_obj':'Y_objStage','Z_obj':'Z_objStage','Image name':'DF1', 'X_image':'X_image', 'Z_image':'Z_image'}
+VARIABLE_HEADER_MAPPING = {'Time':'Time', 'X_obj':'X_objStage','Y_obj':'Y_objStage','Z_obj':'Z_objStage','Image name':'DF1', 'X_image':'X_image', 'Z_image':'Z_image'}
 
 # Older variable mappings (For GM codebase < v2.0)
-VARIABLE_HEADER_MAPPING = {'Time':'Time', 'X_obj':'Xobj','Y_obj':'Yobj','Z_obj':'ZobjWheel','Image name':'Image name', 'X_image':'Xobj_image', 'Z_image':'Zobj'}
+# VARIABLE_HEADER_MAPPING = {'Time':'Time', 'X_obj':'Xobj','Y_obj':'Yobj','Z_obj':'ZobjWheel','Image name':'Image name', 'X_image':'Xobj_image', 'Z_image':'Zobj'}
 
 class Chamber:
 
